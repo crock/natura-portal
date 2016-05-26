e9efe14d776f5c0367eec1283827a2ea443d2774
Alex Crocker <croc122@gmail.com>
2016-05-26 18:12:50 -0400

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+function toggleMenu(x){
+    var panel = document.getElementById(x);
+    if(panel.style.display == "none"){
+        panel.style.display = "inline";
+    } else {
+        panel.style.display = "none";
+    }
+}
+
+function closeMenu(x) {
+	var panel = document.getElementById(x);
+	panel.style.display = "none";
+}
+
+function copytoclipboard() {
+	
+	var clipboard = new Clipboard('#ip');
+
+	clipboard.on('success', function(e) {
+	    console.info('Action:', e.action);
+	    console.info('Text:', e.text);
+	    console.info('Trigger:', e.trigger);
+	
+	    e.clearSelection();
+	    $( "#ip" ).attr( "title", "Copied!" );
+	});
+	
+	clipboard.on('error', function(e) {
+	    console.error('Action:', e.action);
+	    console.error('Trigger:', e.trigger);
+	    $( "#ip" ).attr( "title", "&#8984;+ C to copy" );
+	});
+	
+	new Clipboard('#ip');
+	
+}
+
+
+$("#vote_menu_tab").hover(function() {
+	$("#votingmenu").animate({height:'200px'}, 100, function() { 
+		// Animation complete
+	});
+});
+
+$(".container").hover(function() {
+	$("#votingmenu").animate({height:'0'}, 100, function() { 
+		// Animation complete
+	});
+});
+
+copytoclipboard();
